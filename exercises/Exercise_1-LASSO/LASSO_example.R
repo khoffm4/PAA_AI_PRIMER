41b6b784c1ef3946489b1015b2f7bc5d412dbb27
khoffm4 <bym7444@gmail.com>
2025-04-10 08:53:48 -0400

    exericse 1

--- exercises/Exercise_1-LASSO/LASSO_example.R
+++ exercises/Exercise_1-LASSO/LASSO_example.R
@@ -9,7 +9,7 @@ epsilon = rnorm(n,mean = 0 ,sd=1)
 beta_1 = 0.5
 beta_2 = 0.9
 y = x_1 * beta_1 + x_2 * beta_2 + epsilon
-X = cbind(x_1, x_2,x_3)
+X = cbind(1,x_1, x_2,x_3)
 
 
 #LASSO regression
@@ -21,11 +21,30 @@ plot(fit)
 #LASSO path in matrix form 
 #Each row is the regression coefficient for each variable and each column represents a differnet point on the LASSO path. Compare with plot above to get the full picture
 lass_path_mat = as.matrix(fit$beta)
+lass_path_mat
 
 #LASSO with optimal model chosen by cross validation
-cv_model  = cv.glmnet(X,y)
+cv_model  = cv.glmnet(X,y,alpha=1,standardize = T)
 best_lambda <- cv_model$lambda.min
-best_lambda
 
-best_model <- glmnet(X, y, alpha = 0.1, lambda = best_lambda)
+#Prediction Error over lambda 
+plot(cv_model)
+
+
+best_model <- glmnet(X, y, alpha = 1, lambda = best_lambda)
 coef(best_model)
+
+#In glmnet, lambda is sometimes on the log scale. To align this with the lasso path, take the exponential
+exp(best_lambda)
+
+#Make predictions
+y_hat = predict(best_model, X)
+
+#Compare predictions to true Y
+plot(y_hat, y)
+
+
+
+
+
+
